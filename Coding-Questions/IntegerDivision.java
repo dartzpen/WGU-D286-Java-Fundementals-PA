24e7d41082d8a7774fa656cdfddd54bc00f513f7
Daisy Pennartz <daisypenn99@gmail.com>
2023-11-09 14:24:12 -0600

    IntegerDivision.java update

--- Coding-Questions/IntegerDivision.java
+++ Coding-Questions/IntegerDivision.java
@@ -16,24 +16,33 @@
 // 349
 // the output is:
 // Tables filled: 34
+
 import java.util.Scanner;
 
 public class IntegerDivision {
     public static void main(String[] args) {
+        // create scanner object to retrieve user input
         Scanner scanner = new Scanner(System.in);
+        // declare tableSize variable
         final int tableSize = 10;
+        // use the method GetInput to retreive the user input and store it into the variable 'guests'
         int guests = GetInput(scanner, "Please enter the number of guests: ");
-
+        // use the Calculator method to find the number of filled tables
         int result = Calculator(guests, tableSize);
+        // print the result in the terminal 
         System.out.println("Tables filled: " + result);
     }
     // method that retrieves the user input integer
     public static int GetInput(Scanner scanner, String prompt) {
+        // print the prompt in the terminal 
         System.out.print(prompt);
+        // put the scanner result in the guests variable
         int guests = scanner.nextInt();
+
         return guests;
     }
     public static int Calculator(int guests, int tableSize) {
+        // find the number of tables filled by integer dividing input guests / tableSize (10)
         int tablesFiled = guests / tableSize;
         return tablesFiled;
     }
